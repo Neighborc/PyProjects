3a960f9843a8107134351b278d2901abb0302096
Neighborc <32783284+Neighborc@users.noreply.github.com>
2018-02-05 17:52:51 -0800

    Update HammingDistance.py

--- HammingDistance.py
+++ HammingDistance.py
@@ -1,9 +1,12 @@
 # Chris Neighbor
 # initial python hamming distance sequence not using reservoir computing
 
+# asks user for size of input
 print('How many bits is your input bitstream?:')
 size = input()
 size = int(size)
+
+# gets each bitstream from the user and checks they are the correct length
 while True:
     print(f'Enter a {size} bit stream A:')
     bitstream_A = input()
@@ -13,6 +16,8 @@ while True:
         print(f'Wrong number of bits! Please enter a {size}-bit number for both values')
     elif len(bitstream_A) == size and len(bitstream_B) == size:
         break
+
+# shows the bitstream values and calculates the different bits
 print(f'A = {bitstream_A} \nB =  {bitstream_B}')
 distance = 0
 for bit in range(0,len(bitstream_A)):
